fb1d7c708d880c9bd8e3dfead80cf74f86597ac5
Alejandro Mora <mail@alejandro.md>
2025-10-28 20:25:23 -0400

    feat: ✨ display view counts

--- alejandromd/alejandromd/Services/WordpressRssService.cs
+++ alejandromd/alejandromd/Services/WordpressRssService.cs
@@ -24,15 +24,22 @@ namespace alejandromd.Services
                 reader.Close();
                 var items = feed.Items.Select(item => new RssItem
                 {
+                    Author = item.ElementExtensions.ReadElementExtensions<string>("creator", "http://purl.org/dc/elements/1.1/").FirstOrDefault() ?? string.Empty,
                     Title = item.Title.Text,
                     Description = item.Summary.Text,
                     Link = item.Links.First().Uri.ToString(),
                     PublishDate = item.PublishDate.DateTime,
+                    Views = ParseViews(item.ElementExtensions.ReadElementExtensions<string>("views", "http://alejandro.md/rss/jetpack").FirstOrDefault() ?? "0 views"),
                 }).ToList();
 
                 return Task.FromResult((IEnumerable<RssItem>)items);
             });
             
+        }
+
+        private static int ParseViews(string viewsString)
+        {
+            return int.TryParse(viewsString.Split(' ')[0], out int views) ? views : 0;
         }
     }
 }
