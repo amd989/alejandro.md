4df108678fe2c648e2de2dfa5d6b01089229b755
Alejandro Mora <mail@alejandro.md>
2026-03-22 09:45:17 -0400

    fix: :bug: add cache expiration to the view count

--- alejandromd/alejandromd/Extensions.cs
+++ alejandromd/alejandromd/Extensions.cs
@@ -5,16 +5,20 @@ namespace alejandromd
 {
     public static class Extensions
     {
-        public static async Task<T> GetOrCacheAsync<T>(this IDistributedCache cache, string key, Func<Task<T>> service)
+        public static async Task<T> GetOrCacheAsync<T>(this IDistributedCache cache, string key, Func<Task<T>> service, TimeSpan? expiration = null)
         {
             string? item = await cache.GetStringAsync(key);
             if(!string.IsNullOrEmpty(item))
             {
                 return JsonSerializer.Deserialize<T>(item);
             }
-           
+
             var result = await service();
-            await cache.SetStringAsync(key, JsonSerializer.Serialize(result));
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiration ?? TimeSpan.FromHours(1)
+            };
+            await cache.SetStringAsync(key, JsonSerializer.Serialize(result), options);
             return result;
         }
 
